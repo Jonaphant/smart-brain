4a818ee9a3af19935af8a5615f9bd2a2a7ffb977
Jonathan Sou <jonathansou7@gmail.com>
2020-08-06 17:41:25 -0700

    Updated backend api url.

--- src/container/App.js
+++ src/container/App.js
@@ -10,76 +10,76 @@ import Particles from 'react-particles-js';
 import './App.css';
 
 const particlesOptions = {
-  "particles": {
-    "number": {
-      "value": 150,
-      "density": {
-        "enable": true,
-        "value_area": 800
-      }
-    },
-    "color": {
-      "value": "#ffffff"
-    },
-    "shape": {
-      "type": "circle",
-      "stroke": {
-        "width": 0,
-        "color": "#000000"
+  particles: {
+    number: {
+      value: 150,
+      density: {
+        enable: true,
+        value_area: 800,
       },
-      "polygon": {
-        "nb_sides": 5
+    },
+    color: {
+      value: '#ffffff',
+    },
+    shape: {
+      type: 'circle',
+      stroke: {
+        width: 0,
+        color: '#000000',
       },
-      "image": {
-        "src": "img/github.svg",
-        "width": 100,
-        "height": 100
-      }
+      polygon: {
+        nb_sides: 5,
+      },
+      image: {
+        src: 'img/github.svg',
+        width: 100,
+        height: 100,
+      },
     },
-    "opacity": {
-      "value": 0.5,
-      "random": false,
-      "anim": {
-        "enable": false,
-        "speed": 1,
-        "opacity_min": 0.1,
-        "sync": false
-      }
+    opacity: {
+      value: 0.5,
+      random: false,
+      anim: {
+        enable: false,
+        speed: 1,
+        opacity_min: 0.1,
+        sync: false,
+      },
     },
-    "size": {
-      "value": 3,
-      "random": true,
-      "anim": {
-        "enable": false,
-        "speed": 40,
-        "size_min": 0.1,
-        "sync": false
-      }
+    size: {
+      value: 3,
+      random: true,
+      anim: {
+        enable: false,
+        speed: 40,
+        size_min: 0.1,
+        sync: false,
+      },
     },
-    "line_linked": {
-      "enable": true,
-      "distance": 150,
-      "color": "#ffffff",
-      "opacity": 0.4,
-      "width": 1
+    line_linked: {
+      enable: true,
+      distance: 150,
+      color: '#ffffff',
+      opacity: 0.4,
+      width: 1,
     },
-    "move": {
-      "enable": true,
-      "speed": 5,
-      "direction": "none",
-      "random": false,
-      "straight": false,
-      "out_mode": "out",
-      "bounce": false,
-      "attract": {
-        "enable": false,
-        "rotateX": 600,
-        "rotateY": 1200
-      }
-    }
+    move: {
+      enable: true,
+      speed: 5,
+      direction: 'none',
+      random: false,
+      straight: false,
+      out_mode: 'out',
+      bounce: false,
+      attract: {
+        enable: false,
+        rotateX: 600,
+        rotateY: 1200,
+      },
+    },
   },
-  "retina_detect": true
-}
+  retina_detect: true,
+};
 
 const initialState = {
   input: '',
@@ -92,15 +92,14 @@ const initialState = {
     name: '',
     email: '',
     entries: 0,
-    joined: ''
-  }
-}
+    joined: '',
+  },
+};
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
     this.state = initialState;
   }
-
 
   calculateFaceLocation = (data) => {
     const clarifaiFace = data.outputs[0].data.regions;
@@ -111,109 +110,118 @@ class App extends Component {
     clarifaiFace.forEach((face) => {
       const boundingBoxArr = [];
       const faceCoord = face.region_info.bounding_box;
-      boundingBoxArr.push({leftCol: faceCoord.left_col * width});
-      boundingBoxArr.push({topRow: faceCoord.top_row * height});
-      boundingBoxArr.push({rightCol: width - (faceCoord.right_col * width)});
-      boundingBoxArr.push({bottomRow: height - (faceCoord.bottom_row * height)});
+      boundingBoxArr.push({ leftCol: faceCoord.left_col * width });
+      boundingBoxArr.push({ topRow: faceCoord.top_row * height });
+      boundingBoxArr.push({ rightCol: width - faceCoord.right_col * width });
+      boundingBoxArr.push({
+        bottomRow: height - faceCoord.bottom_row * height,
+      });
       faceCollection.push(boundingBoxArr);
     });
     return faceCollection;
   };
 
-
   displayFaceBox = (box) => {
-    this.setState({box: box});
+    this.setState({ box: box });
   };
 
   loadUser = (data) => {
-    this.setState({user: {
-      id: data.id,
-      name: data.name,
-      email: data.email,
-      entries: data.entries,
-      joined: data.joined
-    }})
+    this.setState({
+      user: {
+        id: data.id,
+        name: data.name,
+        email: data.email,
+        entries: data.entries,
+        joined: data.joined,
+      },
+    });
   };
 
   onInputChange = (event) => {
-    this.setState({input: event.target.value});
+    this.setState({ input: event.target.value });
   };
-  
+
   onButtonSubmit = () => {
-    this.setState({imageUrl: this.state.input});
-    fetch('https://arcane-refuge-98539.herokuapp.com/imageUrl', {
+    this.setState({ imageUrl: this.state.input });
+    fetch('https://smart-brain-backendapi.herokuapp.com/imageUrl', {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        input: this.state.input
-      })
+        input: this.state.input,
+      }),
     })
-      .then(response => response.json())
+      .then((response) => response.json())
       .then((response) => {
-        if(response) {
-          fetch('https://arcane-refuge-98539.herokuapp.com/image', {
+        if (response) {
+          fetch('https://smart-brain-backendapi.herokuapp.com/image', {
             method: 'PUT',
             headers: {
-              'Content-Type': 'application/json'
+              'Content-Type': 'application/json',
             },
             body: JSON.stringify({
-              id: this.state.user.id
+              id: this.state.user.id,
+            }),
+          })
+            .then((res) => res.json())
+            .then((count) => {
+              this.setState(Object.assign(this.state.user, { entries: count }));
             })
-          })
-          .then(res => res.json())
-          .then(count => {
-            this.setState(Object.assign(this.state.user, {entries: count}))
-          })
-          .catch(err => console.log(err));
-        };
+            .catch((err) => console.log(err));
+        }
 
         this.displayFaceBox(this.calculateFaceLocation(response));
       })
       .catch((err) => console.log(err));
 
-      document.querySelector('#detectInput').value = '';
+    document.querySelector('#detectInput').value = '';
   };
 
   onRouteChange = (route) => {
-    if(route === 'signin'){
+    if (route === 'signin') {
       this.setState(initialState);
     } else if (route === 'home') {
-      this.setState({isSignedIn: true});
-    };
-    this.setState({route: route});
+      this.setState({ isSignedIn: true });
+    }
+    this.setState({ route: route });
   };
 
   render() {
     return (
       <div className="App">
-        <Particles className='particles'
-          params={particlesOptions}
-        />
-        <div style={{display: 'flex', justifyContent: 'space-between'}}>
+        <Particles className="particles" params={particlesOptions} />
+        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
           <Logo />
-          <Navigation isSignedIn={this.state.isSignedIn} onRouteChange={this.onRouteChange}/>
+          <Navigation
+            isSignedIn={this.state.isSignedIn}
+            onRouteChange={this.onRouteChange}
+          />
         </div>
-        {this.state.route === 'home'
-          ? <div>
-              <Rank 
-                name={this.state.user.name}
-                entries={this.state.user.entries}
-              />
-              <ImageLinkForm 
-                onInputChange={this.onInputChange} 
-                onButtonSubmit={this.onButtonSubmit}
-              />
-              <br/>
-              <FaceRecognition box= {this.state.box} imageUrl={this.state.imageUrl}/>
-            </div>
-          : (
-              this.state.route === 'signin'
-              ? <SignIn loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
-              : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
-            )
-        }
+        {this.state.route === 'home' ? (
+          <div>
+            <Rank
+              name={this.state.user.name}
+              entries={this.state.user.entries}
+            />
+            <ImageLinkForm
+              onInputChange={this.onInputChange}
+              onButtonSubmit={this.onButtonSubmit}
+            />
+            <br />
+            <FaceRecognition
+              box={this.state.box}
+              imageUrl={this.state.imageUrl}
+            />
+          </div>
+        ) : this.state.route === 'signin' ? (
+          <SignIn loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
+        ) : (
+          <Register
+            loadUser={this.loadUser}
+            onRouteChange={this.onRouteChange}
+          />
+        )}
       </div>
     );
   }
